4ba2fb7677203ba13806796c187fbe5fd06203d0
emdad <emdad@ergo-ventures.com>
2023-06-02 18:07:08 +0600

    Leetcode 1470 shuffle array based on an index from input param

--- src/array/LeetCode_1470_Shuffle_Array.java
+++ src/array/LeetCode_1470_Shuffle_Array.java
@@ -4,7 +4,7 @@ public class LeetCode_1470_Shuffle_Array {
     public int[] shuffle(int[] nums, int n) {
         int[] result = new int[nums.length];
         int i=0;
-        
+
         int j =0;
         for(;i<nums.length;){
            result[i++]=nums[j++];
